53efefb68e63158763812589b38651f9dddea61f
AnastasiyaKos <nastyakos1106@gmail.com>
2019-09-01 23:16:50 +0300

    writing css

---
+++ nerd's/js/script.js
@@ -0,0 +1,47 @@
+let openLetter = document.querySelector('.button-letter');
+let letter = document.querySelector('.modal-login');
+let modalOverlay = document.querySelector('.modal-overlay');
+let closeLetter = document.querySelector('.modal-close');
+let form = letter.querySelector('form');
+
+let userName = letter.querySelector('[name=name]');
+let userEmail = letter.querySelector('[name=email]');
+let userLetter = letter.querySelector('.letter-icon-user');
+
+let isStorageSupport = true;
+let storage = '';
+
+try {
+    storage = localStorage.getItem('');
+} catch (err) {
+    isStorageSupport = false;
+}
+
+openLetter.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log(letter);
+    letter.classList.add('modal-show');
+    modalOverlay.classList.add('modal-show');
+    userName.focus();
+});
+
+closeLetter.addEventListener('click', (e) => {
+    e.preventDefault();
+    letter.classList.remove('modal-show');
+    modalOverlay.classList.remove('modal-show');
+});
+
+
+modalOverlay.addEventListener('click', (e) => {
+    e.preventDefault();
+    letter.classList.remove('modal-show');
+    modalOverlay.classList.remove('modal-show');
+});
+
+form.addEventListener('submit', (e) => {
+    if (!userName.value || !userEmail.value || !userLetter.value) {
+        e.preventDefault();
+        console.log('Enter date')
+    }
+
+});
